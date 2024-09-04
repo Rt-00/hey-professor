1d70bc96a2296f89d3936113871e032fa7823acf
Rafael Toledo <toledorafael304@gmail.com>
2024-09-04 18:33:31 -0300

    HEY-9: should be able to create a new question bigger tham 255 characters

---
+++ tests/Feature/CreateAQuestionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\post;
+
+it('should be able to create a new question bigger tham 255 characters', function () {
+    // Arrange :: Preparar
+    $user = \App\Models\User::factory()->create();
+    actingAs($user);
+
+    // Act
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 260).'?',
+    ]);
+
+    // Assert
+    $request->assertRedirect(route('dashboard'));
+    assertDatabaseCount('questions', 1);
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 260).'?']);
+});
+
+it('should check if ends with question mark ?', function () {});
+
+it('should have at least 10 characters', function () {});
